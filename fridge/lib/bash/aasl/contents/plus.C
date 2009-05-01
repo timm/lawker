4f1b20f5c8eff33b836c0002d5dc181b570e9b57
menzies.tim <menzies.tim@854f1896-e1de-11dd-a893-8d7ce9fbef62>
2009-05-01 03:20:01 +0000

    aaslNew

---
+++ fridge/lib/bash/aasl/contents/plus.C
@@ -0,0 +1,48 @@
+# C-specific kludges incorporated in generated parser by `-b C'
+# mostly typedef handling
+BEGIN {
+	b_nest = 0
+	b_snest = 0
+	b_scope[""] = ""
+}
+function b_ds() {		# declaration start
+	b_nest++
+	b_type[b_nest] = ""
+	b_id[b_nest] = ""
+}
+function b_de() {		# declaration end
+	b_nest--
+}
+function b_dt() {		# declaration is a typedef
+	b_type[b_nest] = "t"
+}
+function b_di() {		# here's the identifier
+	b_id[b_nest] = $2
+}
+function b_dd(   i) {		# declarator seen
+	if (b_id[b_nest] == "")
+		return
+	i = b_id[b_nest]
+	if (b_type[b_nest] == "t") {		# typedef
+		b_oldidtype[b_snest i] = b_idtype[i]
+		b_oldscope[b_snest i] = b_scope[i]
+		b_idtype[i] = "t"
+		b_scope[i] = b_snest
+	} else if (b_idtype[i] == "t") {	# redeclaring typedef name
+		b_oldidtype[b_snest i] = b_idtype[i]
+		b_oldscope[b_snest i] = b_scope[i]
+		b_idtype[i] = ""
+		b_scope[i] = b_snest
+	}
+}
+function b_ss() {		# start scope
+	b_snest++
+}
+function b_se(   id) {		# end scope
+	for (id in b_scope)
+		if (b_scope[id] == b_snest) {
+			b_idtype[id] = b_oldidtype[b_snest id]
+			b_scope[id] = b_oldscope[b_snest id]
+		}
+	b_snest--
+}
